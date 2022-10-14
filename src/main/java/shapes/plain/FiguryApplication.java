e5e2e5e40a0a5721b8a19c75ef3b0dd99f6529f8
Seb96d <seb96d@gmail.com>
2022-10-14 18:05:46 +0200

    Initial commit

---
+++ src/main/java/shapes/plain/FiguryApplication.java
@@ -0,0 +1,13 @@
+package shapes.plain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiguryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiguryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shapes/plain/FiguryApplicationTests.java
@@ -0,0 +1,13 @@
+package shapes.plain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiguryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
